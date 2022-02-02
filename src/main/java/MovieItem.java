6107c79f9cbe9562f3ee7f9b61855efb9899f790
Ilnur Mameshev <imameshev@inbox.ru>
2022-02-02 19:47:02 +0300

    we added some objects

---
+++ src/main/java/MovieItem.java
@@ -0,0 +1,12 @@
+public class MovieItem {
+    private int id;
+    private String ganrName;
+    private String movieName;
+
+
+    public MovieItem(int id, String ganrName, String movieName, int count) {
+        this.id = id;
+        this.ganrName = ganrName;
+        this.movieName = movieName;
+    }
+}
